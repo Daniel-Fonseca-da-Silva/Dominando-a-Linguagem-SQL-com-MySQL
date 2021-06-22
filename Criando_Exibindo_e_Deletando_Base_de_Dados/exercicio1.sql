5a53e1450b8bb667652bea19dafa7e767d5c1f9b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-06-22 14:05:52 -0300

    Criado o arquivo exercicio1

---
+++ Criando_Exibindo_e_Deletando_Base_de_Dados/exercicio1.sql
@@ -0,0 +1,8 @@
+# Exercício do capítulo
+CREATE DATABASE exercicio1; # Crie uma base de dados chamada exercicio1
+
+SHOW DATABASES; # Exiba todas as bases de dados do bd
+
+DROP DATABASE exercicio1; # Delete a base de dados
+
+CREATE DATABASE exercicio1; # Crie uma base de dados chamada exercicio1 novamente
