b79218a2b7b359c025c009b423750c38cb49e6e0
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-06-22 14:04:50 -0300

    Criado arquivo Criar_exibir_deletar_base_de_dados

---
+++ Criando_Exibindo_e_Deletando_Base_de_Dados/Criar_exibir_deletar_base_de_dados.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE mercado; # Cria uma base de dados no bd
+
+SHOW DATABASES; # Exibe todas as bases de dados do bd
+
+DROP DATABASE mercado; # Remove uma base de dados do bd
+
+CREATE DATABASE gravadora; # Cria uma base de dados no bd
+
+SHOW DATABASES; # Exibe todas as bases de dados do bd
+
+USE gravadora; # Ativa a base de dados no bd
