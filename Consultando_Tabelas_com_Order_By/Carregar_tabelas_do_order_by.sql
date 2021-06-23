f7dbe23ee1aae9c6f8d1485a7e5f442527f43631
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-06-23 11:39:00 -0300

    Carregar_tabelas_do_order_by

---
+++ Consultando_Tabelas_com_Order_By/Carregar_tabelas_do_order_by.sql
@@ -0,0 +1,21 @@
+USE empresa;
+
+INSERT INTO funcionarios(id, nome, email, telefone, salario, cargo, pais) 
+VALUES (NULL, 'Marcos Pereira da Silva', 'pereira@gmail.com', '5512532569012', '2500.75', 'Gerente de obras', 'BR');
+
+INSERT INTO funcionarios(id, nome, email, telefone, salario, cargo, pais) 
+VALUES (NULL, 'Lucia López González', 'luci@gmail.com', '5524928647871', '1875.52', 'Secretaria', 'AG');
+
+INSERT INTO funcionarios(id, nome, email, telefone, salario, cargo, pais) 
+VALUES (NULL, 'Fernanda Duarte Machado', 'fefe@gmail.com', '5532526612462', '1500.32', 'Cozinheira', 'BR');
+
+INSERT INTO funcionarios(id, nome, email, telefone, salario, cargo, pais) 
+VALUES (NULL, 'Ana Lima', 'ana@gmail.com', '5524425612395', '2139.52', 'coordenadora', 'BR');
+
+INSERT INTO funcionarios(id, nome, email, telefone, salario, cargo, pais) 
+VALUES (NULL, 'Kin Lan', 'kin@gmail.com', '5524947232562', '2549.59', 'Secretaria', 'CH');
+
+INSERT INTO funcionarios(id, nome, email, telefone, salario, cargo, pais) 
+VALUES (NULL, 'Bruno Siqueira', 'brun@gmail.com', '5532531672457', '1649.69', 'operador de empilhadeira', 'BR');
+
+SELECT * FROM funcionarios;
