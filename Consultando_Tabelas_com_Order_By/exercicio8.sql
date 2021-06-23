2928ffb1ae0e9054fc344cec12f636138fbd0406
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-06-23 11:42:10 -0300

    Criado o arquivo exercicio8

---
+++ Consultando_Tabelas_com_Order_By/exercicio8.sql
@@ -0,0 +1,13 @@
+# Exercício do capítulo 8
+
+# Use a Base de Dados exercicio6
+USE exercicio6;
+
+# Ordene a tabela pela idade dos alunos
+SELECT * FROM alunos ORDER BY idade;
+
+# Ordene a tabela alunos na forma decrescente
+SELECT * FROM alunos ORDER BY idade DESC;
+
+# Ordene a tabela na forma crescente
+SELECT * FROM alunos ORDER BY idade ASC;
